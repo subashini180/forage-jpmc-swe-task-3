0ed2fc111902fea155bf06dedad81c5a5e92146d
subashini180 <subashininatarajan1804@gmail.com>
2024-04-07 13:14:56 +0530

    after changes

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -8,6 +8,8 @@ interface IProps {
   data: ServerRespond[],
 }
 
+
+
 interface PerspectiveViewerElement extends HTMLElement {
   load: (table: Table) => void,
 }
@@ -23,10 +25,13 @@ class Graph extends Component<IProps, {}> {
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
-      timestamp: 'date',
+      price_abc: 'float',
+      price_def: 'float',
+      ratio: 'float',
+      timestamp:'date',
+      upper_bound : 'float',
+      lower_bound:'float',
+      trigger_alert :'float'
     };
 
     if (window.perspective && window.perspective.worker()) {
@@ -36,14 +41,16 @@ class Graph extends Component<IProps, {}> {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
       elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('columns', '["ratio","upper_bound","lower_bound","trigger_alert"]');
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
+        price_abc:'avg',
+        price_def: 'avg',
+        ratio: 'avg',
         timestamp: 'distinct count',
+        upper_bound:'avg',
+        lower_bound:'avg',
+        trigger_alert:'avg'
       }));
     }
   }
@@ -51,7 +58,7 @@ class Graph extends Component<IProps, {}> {
   componentDidUpdate() {
     if (this.table) {
       this.table.update(
-        DataManipulator.generateRow(this.props.data),
+       [ DataManipulator.generateRow(this.props.data)] as unknown as TableData
       );
     }
   }
